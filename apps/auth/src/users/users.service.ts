75aaf112d6a1d52691dde6604d8d010e40de8191
Nanahawaw <folarinyetunde@gmail.com>
2024-12-21 21:32:23 +0100

    apply cookie parser and validate user for signup

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersRepository } from './users.repository';
 import * as bcrypt from 'bcryptjs';
@@ -8,10 +12,22 @@ import { GetUserDto } from './dto/get-user.dto';
 export class UsersService {
   constructor(private usersRepository: UsersRepository) {}
   async createUser(createUser: CreateUserDto) {
+    await this.validateCreateUserDto(createUser);
     return this.usersRepository.create({
       ...createUser,
       password: await bcrypt.hash(createUser.password, 10),
     });
+  }
+
+  private async validateCreateUserDto(createUser: CreateUserDto) {
+    try {
+      await this.usersRepository.findOne({ email: createUser.email });
+    } catch (error) {
+      return;
+    }
+    throw new UnprocessableEntityException(
+      'User with this email already exists',
+    );
   }
 
   async validateUser(email: string, password: string) {
